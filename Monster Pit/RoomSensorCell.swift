562f97e06efcd1c77f707abfa0c0faca963ca8f6
Marc Charbonneau <marc@mbcharbonneau.com>
2015-07-23 17:10:59 -0700

    UI, show updated date.

--- Monster Pit/RoomSensorCell.swift
+++ Monster Pit/RoomSensorCell.swift
@@ -14,10 +14,14 @@ class RoomSensorCell: UICollectionViewCell {
     @IBOutlet weak private var tempLabel: UILabel?
     @IBOutlet weak private var humidityLabel: UILabel?
     @IBOutlet weak private var lightLabel: UILabel?
-
+    @IBOutlet weak private var lastUpdatedLabel: UILabel?
+    
+    private var lastUpdate: NSDate?
+    
     func configureWithSensor( sensor: RoomSensor ) {
 
         nameLabel?.text = sensor.name
+        lastUpdate = sensor.updatedAt
 
         if let degreesC = sensor.temperature {
             tempLabel?.text = "\(celsiusToFahrenheit( degreesC )) ℉"
@@ -26,18 +30,37 @@ class RoomSensorCell: UICollectionViewCell {
         }
         
         if let humidity = sensor.humidity {
-            humidityLabel?.text = "\(humidity * 100.0)%"
+            humidityLabel?.text = "\(humidity * 100.0)% H"
         } else {
-            humidityLabel?.text = "0.0%"
+            humidityLabel?.text = "0.0% H"
         }
         
         if let light = sensor.light {
-            lightLabel?.text = "\(light * 100.0)%"
+            lightLabel?.text = "\(light * 100.0)% L"
         } else {
-            lightLabel?.text = "0.0%"
+            lightLabel?.text = "0.0% L"
+        }
+        
+        updateTimeLabel()
+    }
+    
+    func updateTimeLabel() {
+        
+        let formatter = NSDateComponentsFormatter()
+        formatter.unitsStyle = NSDateComponentsFormatterUnitsStyle.Short
+        formatter.includesApproximationPhrase = false
+        formatter.collapsesLargestUnit = true
+        formatter.maximumUnitCount = 1
+        formatter.allowedUnits = NSCalendarUnit.CalendarUnitMinute | NSCalendarUnit.CalendarUnitSecond | NSCalendarUnit.CalendarUnitDay
+        
+        if let date = lastUpdate {
+            let string = formatter.stringFromDate( date, toDate:NSDate.new() )!
+            lastUpdatedLabel?.text = "Updated \(string) ago"
+        } else {
+            lastUpdatedLabel?.text = ""
         }
     }
-
+    
     private func celsiusToFahrenheit( celsius: Double ) -> Double {
         return celsius * 9 / 5 + 32
     }
--- Monster Pit/RootViewController.swift
+++ Monster Pit/RootViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class RootViewController: UICollectionViewController {
 
     private let dataController = DataController()
+    private let colorGenerator = ColorGenerator()
     private let cellIdentifier = "SensorCell"
-    private var timer: NSTimer?
-
+    private var updateCellsTimer: NSTimer?
+    private var updateDataTimer: NSTimer?
+    
     // MARK: RootViewController
 
     func refreshDataSource( timer: NSTimer ) {
@@ -22,26 +24,41 @@ class RootViewController: UICollectionViewController {
             collectionView?.reloadData()
         }
     }
+    
+    func updateCells( timer: NSTimer ) {
+        
+        if let view = collectionView {
+            
+            for cell in view.visibleCells() as! [RoomSensorCell] {
+                cell.updateTimeLabel()
+            }
+        }
+    }
 
     // MARK: UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        colorGenerator.saturation = 0.3
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
-        timer?.invalidate()
-        timer = NSTimer.scheduledTimerWithTimeInterval(60.0, target:self, selector:Selector("refreshDataSource:"), userInfo:nil, repeats:true)
-        timer?.fire()
+        updateDataTimer?.invalidate()
+        updateDataTimer = NSTimer.scheduledTimerWithTimeInterval(60.0, target:self, selector:Selector("refreshDataSource:"), userInfo:nil, repeats:true)
+        updateDataTimer?.fire()
+        
+        updateCellsTimer?.invalidate()
+        updateCellsTimer = NSTimer.scheduledTimerWithTimeInterval(0.5, target:self, selector:Selector("updateCells:"), userInfo:nil, repeats:true)
     }
 
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
 
-        timer?.invalidate()
+        updateDataTimer?.invalidate()
+        updateCellsTimer?.invalidate()
     }
 
     // MARK: UICollectionViewDataSource
@@ -59,7 +76,10 @@ class RootViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier( cellIdentifier, forIndexPath: indexPath) as! RoomSensorCell
         let sensor = dataController.sensors[indexPath.row]
 
-        cell.backgroundColor = UIColor.redColor()
+        if ( cell.backgroundColor == nil ) {
+            cell.backgroundColor = colorGenerator.randomColor()
+        }
+        
         cell.configureWithSensor( sensor )
         
         return cell
