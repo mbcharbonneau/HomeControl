935d136de691a0927310d1023725edac08373a73
Marc Charbonneau <marc@mbcharbonneau.com>
2015-08-01 15:03:07 -0700

    Sliding button work.

--- Monster Pit/SlidingToggleButton.swift
+++ Monster Pit/SlidingToggleButton.swift
@@ -14,15 +14,27 @@ class SlidingToggleButton: UIControl {
     
     var on: Bool = true {
         didSet {
-            innerView?.backgroundColor = on ? UIColor.redColor() : UIColor.grayColor()
+            titleLabel?.textColor = on ? tintColor : UIColor.lightGrayColor()
+            actionLabel?.text = on ? offActionMessage : onActionMessage
+            innerView?.layer.borderColor = titleLabel?.textColor.CGColor
+            leftView?.backgroundColor = titleLabel?.textColor
             sendActionsForControlEvents(UIControlEvents.ValueChanged)
         }
     }
     
     @IBInspectable var title: String = "" {
-        didSet {
-            titleLabel?.text = title
-        }
+        didSet { titleLabel?.text = title }
+    }
+    
+    @IBInspectable var onActionMessage: String = ""
+    
+    @IBInspectable var offActionMessage: String = "" {
+        didSet { actionLabel?.text = offActionMessage }
+    }
+    
+    @IBInspectable var image: UIImage? {
+        get { return imageView?.image }
+        set { imageView?.image = newValue }
     }
     
     func handlePan(gestureRecognizer: UIPanGestureRecognizer) {
@@ -30,19 +42,21 @@ class SlidingToggleButton: UIControl {
         switch gestureRecognizer.state {
         case .Cancelled, .Ended:
             UIView.animateWithDuration( 0.5, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: UIViewAnimationOptions(0), animations: { () -> Void in
-                    self.innerView!.frame = CGRect(origin: self.bounds.origin, size: self.innerView!.frame.size)
+                self.innerView?.frame = CGRect(origin: self.innerViewOrigin, size: self.innerView!.frame.size)
+                self.actionLabel?.alpha = 0.0
                 }, completion: { (finished: Bool) -> Void in
                     gestureRecognizer.enabled = true
             })
         case .Changed:
             let translation = gestureRecognizer.translationInView(innerView!)
-            let origin = CGPoint(x: translation.x, y: bounds.origin.y)
+            let origin = CGPoint(x: translation.x + innerViewOrigin.x, y: bounds.origin.y)
             
-            if origin.x >= bounds.size.width {
+            if origin.x >= bounds.maxX - hiddenSpacing {
                 on = !on
                 gestureRecognizer.enabled = false
             } else {
-                innerView!.frame = CGRect(origin: origin, size: innerView!.frame.size)
+                innerView?.frame = CGRect(origin: origin, size: innerView!.frame.size)
+                self.actionLabel?.alpha = (hiddenSpacing + origin.x) / hiddenSpacing
             }
         default: ()
         }
@@ -50,30 +64,63 @@ class SlidingToggleButton: UIControl {
     
     // MARK: SlidingToggleButton Private
     
+    private let hiddenSpacing: CGFloat = 90.0
+    private let leftViewWidth: CGFloat = 3.0
+    private let elementSpacing: CGFloat = 5.0
+    private var innerViewOrigin: CGPoint {
+        get { return CGPoint(x: bounds.minX - hiddenSpacing, y: bounds.minY) }
+    }
+    
     private var innerView: UIView?
     private var titleLabel: UILabel?
+    private var actionLabel: UILabel?
+    private var imageView: UIImageView?
+    private var leftView: UIView?
     
     private func configure() {
         
-        titleLabel = UILabel.new()
+        titleLabel = UILabel()
         titleLabel?.text = title
         titleLabel?.setTranslatesAutoresizingMaskIntoConstraints(false)
-        titleLabel?.font = UIFont.systemFontOfSize(14.0)
-        titleLabel?.textColor = UIColor.whiteColor()
+        titleLabel?.font = UIFont.systemFontOfSize(16.0)
+        titleLabel?.textColor = tintColor
+        titleLabel?.textAlignment = NSTextAlignment.Left
         
+        imageView = UIImageView()
+        imageView?.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        leftView = UIView()
+        leftView?.setTranslatesAutoresizingMaskIntoConstraints(false)
+        leftView?.backgroundColor = tintColor
+        leftView?.layer.cornerRadius = leftViewWidth / 2.0
+        
+        actionLabel = UILabel()
+        actionLabel?.setTranslatesAutoresizingMaskIntoConstraints(false)
+        actionLabel?.numberOfLines = 2
+        actionLabel?.font = UIFont.systemFontOfSize(12.0)
+        actionLabel?.textColor = UIColor.darkGrayColor()
+        actionLabel?.alpha = 0.0
+  
         let pan = UIPanGestureRecognizer(target: self, action: Selector("handlePan:"))
-        let frame = CGRect(origin: bounds.origin, size: CGSize(width: 100.0, height: bounds.height))
+        let frame = CGRect(origin: innerViewOrigin, size: CGSize(width: 240.0, height: bounds.height))
         
         innerView = UIView(frame: frame)
-        innerView?.backgroundColor = UIColor.redColor()
         innerView?.addSubview(titleLabel!)
+        innerView?.addSubview(imageView!)
+        innerView?.addSubview(leftView!)
+        innerView?.addSubview(actionLabel!)
         innerView?.addGestureRecognizer(pan)
 
-        let views: [String: UIView] = ["titleLabel": titleLabel!]
+        let metrics: [String: CGFloat] = ["imageSize": bounds.height, "leftViewWidth": leftViewWidth, "space": elementSpacing, "hidden": hiddenSpacing]
+        let views: [String: UIView] = ["titleLabel": titleLabel!, "imageView": imageView!, "leftView": leftView!, "actionLabel": actionLabel!]
         
-        innerView?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-5-[titleLabel]|", options: NSLayoutFormatOptions(0), metrics: nil, views: views))
-        innerView?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[titleLabel]|", options: NSLayoutFormatOptions(0), metrics: nil, views: views))
+        innerView?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[actionLabel(hidden)]-(space)-[leftView(leftViewWidth)]-(space)-[imageView(imageSize)]-(space)-[titleLabel]|", options: NSLayoutFormatOptions(0), metrics: metrics, views: views))
+        innerView?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[titleLabel]|", options: NSLayoutFormatOptions(0), metrics: metrics, views: views))
+        innerView?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[imageView(imageSize)]|", options: NSLayoutFormatOptions(0), metrics: metrics, views: views))
+        innerView?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[leftView]|", options: NSLayoutFormatOptions(0), metrics: metrics, views: views))
+        innerView?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[actionLabel]|", options: NSLayoutFormatOptions(0), metrics: metrics, views: views))
 
+        backgroundColor = UIColor.clearColor()
         addSubview(innerView!)
     }
     
